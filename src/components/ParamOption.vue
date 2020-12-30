19fae491157d640b13fe585ed9576630ddbba01b
Andrew Mauney <andrew.e.mauney@gmail.com>
2020-12-30 18:45:41 -0500

    Refactor metafields params display

--- src/components/ParamOption.vue
+++ src/components/ParamOption.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="option" v-show="isMetafield">
-    {{ paramData }}
-  </div>
+  <polaris-vue-fade-transition>
+    <div class="option">
+      {{ namespaceName }} - {{ keyName }} ({{ typeName }})
+      <polaris-button destructive @click="removeFxn(paramData)">Remove</polaris-button>
+    </div>
+  </polaris-vue-fade-transition>
 </template>
 
 <script>
@@ -17,12 +20,35 @@ export default {
     }
   },
   computed: {
-    isMetafield: function() {
-      console.log(this.paramData);
-      return this.paramData.indexOf(":") > -1 && this.paramData.indexOf("metafields") > -1
+    namespaceName: function() {
+      return this.paramData.split('.')[1]
+    },
+    keyName: function() {
+      return this.paramData.split('.')[2].split(':')[0]
+    },
+    typeName: function() {
+      return this.paramData.split('.')[2].split(':')[1]
     },
   },
   methods: {
   }
 }
 </script>
+
+<style lang="scss">
+#app {
+
+  .option {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1.6rem;
+    text-align: left;
+
+    .Polaris-Button {
+      margin-top: 0;
+    }
+  }
+
+}
+</style>
