7d6abc0dbecc13ff8d42119a315272f1c6c496f3
Andrew Mauney <andrew.e.mauney@gmail.com>
2020-12-31 16:23:05 -0500

    more public path tweaking

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   css: {
     sourceMap: true,
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/bulk-editor-builder/'
-      : '/'
-  }
+    publicPath: '/bulk-editor-builder/'
+  },
+
+  publicPath: '/bulk-editor-builder/'
 }
